34134578ebc9f4b77fc9cbaf30fd6113f92ce3f9
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-11-04 01:25:11 +0700

    finalizing mail

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from mail.mail import Email
+from mail.enums.server_type import ServerType
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if __name__ == '__main__':
+    email = Email(
+        mail_to=["tontan2545@gmail.com"],
+        mail_from="medicallnoreply@gmail.com",
+        password=os.getenv("GMAIL_PASSWORD"),
+        subject="Test",
+    )
+    email.send()
